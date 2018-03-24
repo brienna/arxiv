7252181f559fdb4283d53d3c0239b7d92063d41d
Brienna Herold <brienna.herold@gmail.com>
2018-03-23 21:40:17 -0400

    Add documentation

--- download_arxiv_data.py
+++ download_arxiv_data.py
@@ -2,75 +2,114 @@ import boto3, tarfile, os, shutil, datetime
 
 s3resource = None
 
-def main():
-    global s3resource
-    global destination_bucket
+def setup():
+    """Sets up data download"""
+
     # Tell boto3 we want to use the resource from Amazon S3
+    global s3resource
     s3resource = boto3.resource("s3")
-    # Initialize source bucket
+    # Set source bucket name
     source_bucket = 'arxiv'
 
-    # Collect all files for s3://arxiv/src/<file> 
+    # Fetch file list for s3://arxiv/src directory
     # Note: Limited to 1,000, need Paginator to get more?
     objects = s3resource.meta.client.list_objects(
         Bucket=source_bucket,
         RequestPayer='requester',
         Prefix='src/')
 
-    # Copy file (only one for now)
+    # Download and extract .tar files that haven't been downloaded & extracted yet
+    # Note: Only downloading one for now
     for file in objects["Contents"][:1]:
         key = file["Key"]
         if key.endswith('.tar'):
-            # UNCOMMENT ONCE ALREADY GET THE FILE, OR DO CHECK FOR EXISTING
-            downloadFile(source_bucket, key) 
-            # copyFileToS3(source_bucket, key)
-            pass
-
-    # unzip .tar file
-    extractFile(key)
+            if os.path.exists(key):
+                print(key + ' already has been downloaded and extracted')
+                continue;
+            else:
+                downloadFile(source_bucket, key)
+                extractFile(key)
+                parseFiles()
+    
+    # copyFileToS3(source_bucket, key)
 
 
 def copyFileToS3(source_bucket, key):
-    destination_bucket = 'briennakh-arxiv'
+    """
+    Copies file from source bucket to specified S3 bucket
 
+    Parameters
+    ----------
+    source_bucket : str
+        Name of source bucket
+    key : str
+        Name of file to copy
+    """
+
+    # Set destination bucket name for file to be copied to
+    destination_bucket = 'briennakh-arxiv'
     print("Copying s3://{}/{} to s3://{}/{}...".format(
         source_bucket,
         key,
         destination_bucket,
         key))
 
-    # Copy file (source key) from source bucket 
-    # to destination bucket, naming it destination key
+    # Copy file from source bucket to destination bucket
     response_copy = s3resource.Object(
         destination_bucket,key).copy_from(
                 CopySource='{}/{}'.format(source_bucket, key),
                 RequestPayer='requester')
 
-    # Check that it copied successfully
+    # Check that file copied successfully
     if response_copy["ResponseMetadata"]["HTTPStatusCode"] == 200:
-        print("SUCCESS")
+        print("SUCCESS COPYING " + key + " to " + destination_bucket)
 
 
 def downloadFile(source_bucket, key):
+    """
+    Downloads file from source bucket to computer
+
+    Parameters
+    ----------
+    source_bucket : str
+        Name of source bucket
+    key : str
+        Name of file to download
+    """
+
     # Ensure 'src' folder exists
     if not os.path.isdir('src'):
         os.makedirs('src')
 
+    # Download file
     print("Downloading s3://{}/{} to {}...".format(
         source_bucket,
         key, 
         key))
-    s3resource.meta.client.download_file(source_bucket, key, key, {'RequestPayer':'requester'})
+    s3resource.meta.client.download_file(
+        source_bucket, 
+        key, 
+        key, 
+        {'RequestPayer':'requester'})
 
 
 def extractFile(filename):
-    # Add date to error log
+    """
+    Extracts specified file
+
+    Parameters
+    ----------
+    filename : str
+        Name of file to extract
+    """
+
+    # Start new section in error log
     log = open('error_log.txt', 'a')
     log.write('\n' + datetime.datetime.now().isoformat() + '\n')
 
-    # Process if filename is a .tar file
+    # Proceed with file extraction if .tar
     if not tarfile.is_tarfile(filename):
-        print('can\'t unzip, not a .tar file')
+        print('can\'t unzip ' + filename + ', not a .tar file')
         return
     print('Processing ' + filename + '...')
     tar = tarfile.open(filename)
@@ -88,7 +127,7 @@ def extractFile(filename):
                         gz.extract(subsubfile, path='latex')
                 print(subfile.name + ' extracted successfully')
             except tarfile.ReadError:
-                # Append subfile name to error log
+                # If error extracting subfile, register in error log
                 print('error extracting ' + subfile.name + '...')
                 log.write(subfile.name + '\n')
     tar.close()
@@ -96,18 +135,9 @@ def extractFile(filename):
     print(filename + ' processed successfully')
 
 
-# If file is being run as a script on the command line
 if __name__ == '__main__':
-    main()
+    """Runs if script called on command line."""
+    setup()
 
-
-
-# NEXT STEPS:
-# 1) Why are many .gz files throwing ReadErrors?
-# 2) Do I need to actually extract the .tar and all the .gz?
-# 3) Check each .tex file and only extract the ones that start with \document
-# 4) In a different program, parse each .tex file for words (with latex module?)
-# 5) Check if have existing .tar before downloading & writing over
-# 6) Close any gz/tar files? 
 
 
